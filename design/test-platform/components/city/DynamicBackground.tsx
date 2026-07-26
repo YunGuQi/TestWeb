cfdef16aa500deb4d347f0ba34297a96df4559db
e <12091254987@qq.com>
2026-07-26 18:11:29 +0800

    fix(city): convert topo lines to vh for mobile responsiveness

--- design/test-platform/components/city/DynamicBackground.tsx
+++ design/test-platform/components/city/DynamicBackground.tsx
@@ -20,16 +20,15 @@ export default function DynamicBackground() {
            }}
       />
 
-      {/* 缓慢旋转的抽象弧线（完美复刻 HTML 中因 CSS 错位产生的 Y=150vh 下沉式巨型弧线美感） */}
+      {/* 缓慢旋转的抽象弧线（手机与电脑双端适配版） */}
       <div className="absolute inset-0 pointer-events-none z-10 overflow-hidden">
-        {/* 将锚点放置在屏幕垂直 150% 的位置，水平居中。
-            这样就能完美重现 HTML 原型里，因为 content-b 将 content-c 向下挤压了一屏高度，
-            导致圆心跑到屏幕正下方，从而在屏幕内只露出巨大、舒展边缘弧线的神级视觉效果。 */}
+        {/* 将锚点放置在屏幕垂直 150% 的位置，水平居中。*/}
         <div className="absolute w-0 h-0 top-[150%] left-1/2">
-          <div className="absolute w-[150vw] h-[150vw] -left-[75vw] -top-[75vw] rounded-[40%_60%_70%_30%/40%_50%_60%_50%] border border-[#e6e4df] opacity-10 animate-[spin_40s_linear_infinite]" />
-          <div className="absolute w-[120vw] h-[120vw] -left-[60vw] -top-[60vw] rounded-[60%_40%_30%_70%/50%_60%_40%_50%] border border-[#e6e4df] opacity-[0.15] animate-[spin_35s_linear_infinite_reverse]" />
-          <div className="absolute w-[90vw] h-[90vw] -left-[45vw] -top-[45vw] rounded-[30%_70%_70%_30%/30%_30%_70%_70%] border border-[#e6e4df] opacity-20 animate-[spin_30s_linear_infinite]" />
-          <div className="absolute w-[60vw] h-[60vw] -left-[30vw] -top-[30vw] rounded-[50%_50%_20%_80%/25%_75%_25%_75%] border border-[#e6e4df] opacity-[0.25] animate-[spin_25s_linear_infinite_reverse]" />
+          {/* 使用 vh 代替 vw，确保在手机端（屏幕很窄时）圆的半径依然足够大，能够从 150% 底部升起并划过整个屏幕 */}
+          <div className="absolute w-[270vh] h-[270vh] -left-[135vh] -top-[135vh] rounded-[40%_60%_70%_30%/40%_50%_60%_50%] border border-[#e6e4df] opacity-10 animate-[spin_40s_linear_infinite]" />
+          <div className="absolute w-[210vh] h-[210vh] -left-[105vh] -top-[105vh] rounded-[60%_40%_30%_70%/50%_60%_40%_50%] border border-[#e6e4df] opacity-[0.15] animate-[spin_35s_linear_infinite_reverse]" />
+          <div className="absolute w-[160vh] h-[160vh] -left-[80vh] -top-[80vh] rounded-[30%_70%_70%_30%/30%_30%_70%_70%] border border-[#e6e4df] opacity-20 animate-[spin_30s_linear_infinite]" />
+          <div className="absolute w-[110vh] h-[110vh] -left-[55vh] -top-[55vh] rounded-[50%_50%_20%_80%/25%_75%_25%_75%] border border-[#e6e4df] opacity-[0.25] animate-[spin_25s_linear_infinite_reverse]" />
         </div>
       </div>
 
